f69c884104092c694e328bf65b5f11ed37cfa864
Chitrank-Dixit <chitrankdixit@gmail.com>
2014-08-06 00:32:31 +0530

    Program to check a number is positive or negative has been added to the repository

---
+++ Fundamental_Programs/positive_or_negative.c
@@ -0,0 +1,18 @@
+/* C program to check whether a given number is positive or negative */
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter any number to check whether it is positive or negative: ");
+    scanf("%d", &num);
+    if (num < 0)
+    {
+        printf("The number %d is negative", num);
+    }
+    else
+    {
+        printf("The number %d is positive", num);
+    }
+    return 0;
+
+}
